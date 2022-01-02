318faee03c50df4b99d3a4c7d6ee7be5d01393a2
Marcos Bruno <contato@marcosbruno.com>
2022-01-02 08:57:04 -0300

    return techniques from a group and subgroups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
 
-
 	validates :name, presence: true
 
 	validate :major_group_cannot_be_zero
@@ -16,6 +15,19 @@ class Group < ApplicationRecord
 		if !major_group.blank? and major_group == id
 			errors.add(:major_group, "Major group cannot reference itself")
 		end
+	end
+
+	def techiniques
+		Technique.where(group: self.subgroups)		
+	end
+
+	def subgroups(subgroups_array = [])
+		subgroups = Group.where(major_group: id)
+		subgroups_array += subgroups.all
+		subgroups.each do |group|
+			group.subgroups(subgroups_array)
+		end
+		subgroups_array
 	end
 
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -19,4 +19,36 @@ RSpec.describe Group, type: :model do
     expect(minor_group.major_group).to equal(major_group.id)
   end
 
+
+  it "return all techniques subgroups" do
+    nagewaza = Group.create(name: "Nage-waza")
+    tewaza = Group.create(name: "Te-waza", major_group: nagewaza.id)
+    ashiwaza = Group.create(name: "Ashi-waza", major_group: nagewaza.id)
+    expect(nagewaza.subgroups.pluck(:id)).to eq([tewaza.id, ashiwaza.id])
+  end
+
+
+  it "return all techniques from a group and subgroups" do
+
+    nagewaza = Group.create(name: "Nage-waza")
+    tewaza = Group.create(name: "Te-waza", major_group: nagewaza.id)
+    ashiwaza = Group.create(name: "Ashi-waza", major_group: nagewaza.id)
+
+    tewaza_techiniques = Technique.create([
+      {name: "Seoi-nage", group_id: tewaza.id},
+      {name: "Ippon-seoi-nage", group_id: tewaza.id},
+      {name: "Seoi-otoshi", group_id: tewaza.id},
+    ]).pluck(:id)
+
+    ashiwaza_techiniques = Technique.create([
+      {name: "O-soto-gari", group_id: ashiwaza.id},
+      {name: "Ashi-harai", group_id: ashiwaza.id},
+      {name: "Uchi-mata", group_id: ashiwaza.id},
+    ]).pluck(:id)
+
+    nagewaza_techiniques = tewaza_techiniques + ashiwaza_techiniques
+
+    expect(nagewaza.techiniques.pluck(:id)).to eq(nagewaza_techiniques)    
+  end
+
 end
